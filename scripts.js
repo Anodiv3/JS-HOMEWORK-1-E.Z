82d4676d98f3330ac920cda3cf695b0867b80224
Anodiv3 <zibrovskaelizaveta@gmail.com>
2023-12-27 15:42:03 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,73 @@
+console.log('Elizaveta Zibrosvka');
+console.log('14.06.2002');
+console.log(14,0o6,2002);
+
+console.log('Добро'+' пожаловать'+' на курс');
+
+/* alert(2019); */
+
+let x = -200;
+let y = 2019;
+console.log(y + x);
+
+let xX = 2019 -200;
+console.log(xX);
+
+let hello = document.getElementById("divSix");
+    hello.innerHTML += `<p>Hello World!</p>`;
+
+let multiplication = document.getElementById("divSeven");
+    let xy = 12 * 12;
+        multiplication.innerHTML += `<p>` + xy + `</p>`;
+
+const eight = document.querySelector(".eight");
+    eight.innerHTML += `<p>Task 8</p>`;
+
+const nine = document.querySelector(".nine");
+    nine.innerHTML += `<h2>Hello <span class="eight">everyone</span></h2>`;
+
+const ten = document.querySelector(".ten");
+    ten.innerHTML += `<h2>Hello</h2>`
+
+const eleven = document.querySelector(".eleven");
+    eleven.innerHTML += `123 ` + ` 456`;
+
+const twelve = document.querySelector(".twelve");
+    const a = 3.1415;
+        twelve.innerHTML += '<p>' + a + '</p>';
+            console.log(a);
+
+const thirteen = document.querySelector(".thirteen")
+    thirteen.innerHTML += '<img src="https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg" alt="js">'; // Я поменяла картинку с йоды на кота потому что йода не отображался
+
+const fourteen = document.querySelector(".fourteen")
+    let z1 = 6, z2 = 3;
+/* let multiplicationZ = z1 * z2; */
+
+fourteen.innerHTML += `<p>` + z1 * z2 + `</p>`;
+
+const fifteen = document.querySelector(".fifteen")
+    let y1 = 6, y2 = 3;
+
+fifteen.innerHTML += `<p>` + y1 / y2 + `</p>`
+
+const sixteen = document.querySelector(".sixteen")
+    let x1='Hello ', x2 = 5;
+
+sixteen.innerHTML += `<p>` + x1 + x2 + `</p>`
+
+const seventeen = document.querySelector(".seventeen");
+    console.log(seventeen)
+
+let eighteen = document.querySelector(".eighteen")
+    eighteen = 5;
+
+console.log(eighteen)
+
+let nineteen = document.querySelector(".nineteen");
+    nineteen = document.querySelector(".nineteen-testt");
+    
+console.log(nineteen)
+
+const twenty = document.querySelector(".twenty");
+    twenty.textContent += '<h2>Hi</h2>';
